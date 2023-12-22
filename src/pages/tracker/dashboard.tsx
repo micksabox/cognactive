de800305445582e1a3be852b767a6354a1a4d2fc
Michael Nolivos <michaelnolivos@gmail.com>
2023-12-22 01:35:19 -0500

    feat: initial persistence

--- src/pages/tracker/dashboard.tsx
+++ src/pages/tracker/dashboard.tsx
@@ -1,8 +1,9 @@
 import { differenceInCalendarDays, addMonths } from 'date-fns'
-import { Moon, Sun } from 'lucide-react'
+import { CheckCircle, Circle, Moon, Sun } from 'lucide-react'
 import { Button } from 'src/components/ui/button'
 import { Progress } from 'src/components/ui/progress'
 import DieOffSymptoms from './die-off-symptoms'
+import useLocalStorageState from 'src/hooks/use-localstorage-state'
 
 interface DashboardProps {
   startDate: Date
@@ -11,9 +12,48 @@ interface DashboardProps {
 const Dashboard: React.FC<DashboardProps> = ({ startDate }) => {
   const today = new Date()
   const start = startDate
+  const dateKey = today.toISOString().split('T')[0]
   const dayNumber = differenceInCalendarDays(today, start)
   const twoMonthsLater = addMonths(start, 2)
   const daysUntilTwoMonths = differenceInCalendarDays(twoMonthsLater, today)
+
+  type RegimenActivities = {
+    oreganoOil: boolean
+    nac: boolean
+    blackSeedOil: boolean
+    nightOreganoOil: boolean
+    nightNac: boolean
+    nightBlackSeedOil: boolean
+  }
+
+  type ActivitiesState = Record<string, RegimenActivities>
+
+  const [activities, setActivities] = useLocalStorageState<ActivitiesState>('nactivities', {})
+
+  const saveActivityState = (activityName: keyof RegimenActivities, state: boolean) => {
+    const currentDayActivities = activities[dateKey] || {
+      morning: false,
+      nighttime: false,
+      oreganoOil: false,
+      nac: false,
+      blackSeedOil: false,
+      nightOreganoOil: false,
+      nightNac: false,
+      nightBlackSeedOil: false,
+    }
+
+    const updatedActivities = {
+      ...activities,
+      [dateKey]: {
+        ...currentDayActivities,
+        [activityName]: state,
+      },
+    }
+
+    setActivities(updatedActivities)
+  }
+
+  const todaysActivities = activities[dateKey] || {}
 
   return (
     <div>
@@ -31,18 +71,45 @@ const Dashboard: React.FC<DashboardProps> = ({ startDate }) => {
       <h3 className="my-4 text-center text-2xl font-light">Your Daily Regimen</h3>
       <div className="grid grid-cols-2 gap-2">
         <Button>
-          Morning <Sun className="w-4 text-yellow-300" />
+          Morning <Sun className="w-4 text-yellow-300" />{' '}
+          {todaysActivities &&
+            todaysActivities.oreganoOil &&
+            todaysActivities.nac &&
+            todaysActivities.blackSeedOil &&
+            'Complete'}
         </Button>
         <Button>
           Nightime
-          <Moon className="w-4 text-blue-400" />
+          <Moon className="w-4 text-blue-400" />{' '}
+          {todaysActivities &&
+            todaysActivities.nightOreganoOil &&
+            todaysActivities.nightNac &&
+            todaysActivities.nightBlackSeedOil &&
+            'Complete'}
         </Button>
-        <button className="border p-2">Oregano Oil</button>
-        <button className="border p-2">Oregano Oil</button>
-        <button className="border p-2">NAC</button>
-        <button className="border p-2">NAC</button>
-        <button className="border p-2">Black Seed Oil</button>
-        <button className="border p-2">Black Seed Oil</button>
+        <Button
+          onClick={() => saveActivityState('oreganoOil', !todaysActivities.oreganoOil)}
+          variant={'outline'}
+          className="border p-2"
+        >
+          {todaysActivities.oreganoOil ? <CheckCircle className="w-4 text-green-600" /> : <Circle className="w-4" />}
+          Oregano Oil
+        </Button>
+        <Button variant={'outline'} className="border p-2">
+          Oregano Oil
+        </Button>
+        <Button variant={'outline'} className="border p-2">
+          NAC
+        </Button>
+        <Button variant={'outline'} className="border p-2">
+          NAC
+        </Button>
+        <Button variant={'outline'} className="border p-2">
+          Black Seed Oil
+        </Button>
+        <Button variant={'outline'} className="border p-2">
+          Black Seed Oil
+        </Button>
       </div>
       <h3 className="my-4 text-center text-2xl font-light">How do you feel?</h3>
       <DieOffSymptoms />
