bdf20a56d40d0b8d092c782e05ff6c5da9052c5c
Michael Nolivos <michaelnolivos@gmail.com>
2023-12-26 16:21:34 -0500

    feat: new cognactive app icon

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,20 @@
 import React, { ReactNode } from 'react'
-import { LanguageSelector } from '../language-selector'
-import { useTranslation } from 'react-i18next'
 
+import CognactiveIcon from 'src/assets/icons/cognactive-icon.svg'
 interface IProps {
   leftNode?: ReactNode
 }
 export function Header(props: IProps) {
-  const { t } = useTranslation()
-
   return (
     <div className="flex w-full items-center justify-between border bg-slate-50 bg-opacity-70 px-4 py-4 md:px-12">
-      <a href="/" className="text-base font-semibold">
-        {t('title')}
+      <a href="/" className="flex items-center text-base font-semibold">
+        <span className="mr-2 inline-block w-8">
+          <CognactiveIcon />
+        </span>
+        cognactive
       </a>
       <div className="flex items-center gap-4">
-        <LanguageSelector />
+        {/* <LanguageSelector /> */}
         {/* <Button asChild>
           <a href="https://github.com/Quilljou/vite-react-ts-tailwind-starter" target="_blank" rel="noreferrer">
             View Code &nbsp;
