79e9f5c185d157df8fb64e83df110bfcc548472f
Michael Nolivos <michaelnolivos@gmail.com>
2023-12-28 00:57:12 -0500

    feat: hero animation and text

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,5 +1,7 @@
-import { Rocket, Globe2, Wrench, Zap } from 'lucide-react'
+import { Zap, ActivitySquare, Globe2 } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
+import CognactiveIcon from 'src/assets/icons/cognactive-icon.svg'
+
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
 
@@ -8,11 +10,14 @@ export const Hero = () => {
 
   return (
     <div className="flex min-h-screen bg-gradient-to-b from-slate-600 to-slate-900">
-      <section className="w-full py-32 md:py-48">
+      <section className="w-full py-16 md:py-24">
         <div className="container px-4 md:px-6">
           <div className="grid items-center gap-6">
             <div className="flex flex-col justify-center space-y-4 text-center">
               <div className="mb-24">
+                <div id="cognactive-symbol" className="defence-falling m-auto w-48">
+                  <CognactiveIcon />
+                </div>
                 <h1 className="mb-6 text-3xl font-bold tracking-tighter text-transparent text-white sm:text-5xl xl:text-6xl/none">
                   {t('hero-title')}
                 </h1>
@@ -27,10 +32,10 @@ export const Hero = () => {
                 <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
                   <div className="flex flex-col items-center space-y-2 rounded-lg p-4">
                     <div className="rounded-full bg-black p-4 text-white">
-                      <Rocket size={32} />
+                      <ActivitySquare size={32} />
                     </div>
-                    <h2 className="text-2xl font-bold text-white">{t('modernstack')}</h2>
-                    <p className="text-white">{t('modernstack-desc')}</p>
+                    <h2 className="text-2xl font-bold text-white">{t('tracking')}</h2>
+                    <p className="text-white">{t('tracking-desc')}</p>
                   </div>
                   <div className="flex flex-col items-center space-y-2 rounded-lg p-4">
                     <div className="rounded-full bg-black p-4 text-white">
@@ -39,13 +44,13 @@ export const Hero = () => {
                     <h2 className="text-2xl font-bold text-white">{t('i18nsupport')}</h2>
                     <p className="text-white">{t('i18nsupport-desc')}</p>
                   </div>
-                  <div className="flex flex-col items-center space-y-2 rounded-lg p-4">
-                    <div className="rounded-full bg-black p-4 text-white">
-                      <Wrench size={32} />
+                  {/* <div className="flex flex-col items-center p-4 space-y-2 rounded-lg">
+                    <div className="p-4 text-white bg-black rounded-full">
+                      <HeartHandshake size={32} />
                     </div>
                     <h2 className="text-2xl font-bold text-white"> {t('linters')} </h2>
                     <p className="text-white">{t('linters-desc')}</p>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
