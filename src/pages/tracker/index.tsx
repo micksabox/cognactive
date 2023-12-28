5d30d89c3f8459692b20898f10cfd0db30fceecf
Michael Nolivos <michaelnolivos@gmail.com>
2023-12-27 23:09:05 -0500

    feat: restrict to standalone mobile app

--- src/pages/tracker/index.tsx
+++ src/pages/tracker/index.tsx
@@ -7,14 +7,36 @@ import { formatDateKey } from 'src/lib/utils'
 import db from './db'
 import { toast } from 'react-hot-toast'
 import CognactiveIcon from 'src/assets/icons/cognactive-icon.svg'
+import useIsAppInstalled from './use-is-app-installed'
+import { Alert, AlertDescription, AlertTitle } from 'src/components/ui/alert'
+import { LayoutGrid } from 'lucide-react'
 
 const ProtocolTracker: React.FC = () => {
   const [startDate, setStartDate] = useLocalStorageState<string | null>('protocol_start_date', null)
   const [pickerDate, setPickerDate] = useState<Date | undefined>()
+  const isAppInstalled = useIsAppInstalled()
 
   const handleStartProtocol = () => {
-    const currentDate = formatDateKey(pickerDate || new Date())
-    setStartDate(currentDate)
+    if (isAppInstalled) {
+      const currentDate = formatDateKey(pickerDate || new Date())
+      setStartDate(currentDate)
+    } else {
+      const alertElement = document.getElementById('app-install-alert')
+      if (alertElement) {
+        alertElement.animate(
+          [
+            { transform: 'scale(1)', opacity: 1, offset: 0 },
+            { transform: 'scale(1.05)', opacity: 1, offset: 0.3 },
+            { transform: 'scale(1)', opacity: 1, offset: 1 },
+          ],
+          {
+            duration: 500,
+            easing: 'ease-in-out',
+            fill: 'both',
+          },
+        )
+      }
+    }
   }
 
   return (
@@ -62,6 +84,17 @@ const ProtocolTracker: React.FC = () => {
           <Button size={'lg'} className="mt-4 w-full" onClick={handleStartProtocol}>
             Start Tracking NAC Protocol
           </Button>
+          {!isAppInstalled && (
+            <Alert id="app-install-alert" className="mt-2">
+              <LayoutGrid className="w-4" />
+              <AlertTitle>Install cognactive to mobile device</AlertTitle>
+              <AlertDescription>
+                cognactive is designed to be used as a mobile app during the protocol. Open this webpage from your
+                mobile device. Then open the Share menu and select Add to Home Screen.
+              </AlertDescription>
+            </Alert>
+          )}
+
           <div className="mt-2 text-sm">
             By proceeding you agree to:
             <p className="font-semibold">Privacy Policy</p>
---
+++ src/pages/tracker/use-is-app-installed.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+
+const useIsAppInstalled = () => {
+  const [isAppInstalled, setIsAppInstalled] = useState(false)
+
+  useEffect(() => {
+    // Detecting iOS
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
+    // @ts-ignore
+    if (isIOS && window.navigator.standalone) {
+      setIsAppInstalled(true)
+      return
+    }
+
+    // Detecting Android
+    const isAndroid = /Android/.test(navigator.userAgent)
+    if (isAndroid) {
+      const mediaQuery = window.matchMedia('(display-mode: standalone)')
+      const checkDisplayMode = () => setIsAppInstalled(mediaQuery.matches)
+
+      checkDisplayMode() // Check on initial load
+
+      // Listen for changes in display mode
+      mediaQuery.addListener(checkDisplayMode)
+
+      // Cleanup
+      return () => mediaQuery.removeListener(checkDisplayMode)
+    }
+  }, [])
+
+  return isAppInstalled
+}
+
+export default useIsAppInstalled
