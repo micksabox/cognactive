a188bbf8bd57e72156d06f79edb27b4ff1577943
Michael Nolivos <michaelnolivos@gmail.com>
2023-12-27 23:27:54 -0500

    feat: improved tracker onboarding

--- src/pages/tracker/index.tsx
+++ src/pages/tracker/index.tsx
@@ -8,7 +8,7 @@ import db from './db'
 import { toast } from 'react-hot-toast'
 import useIsAppInstalled from './use-is-app-installed'
 import { Alert, AlertDescription, AlertTitle } from 'src/components/ui/alert'
-import { LayoutGrid } from 'lucide-react'
+import { LayoutGrid, ShareIcon } from 'lucide-react'
 
 const ProtocolTracker: React.FC = () => {
   const [startDate, setStartDate] = useLocalStorageState<string | null>('protocol_start_date', null)
@@ -84,21 +84,23 @@ const ProtocolTracker: React.FC = () => {
               <LayoutGrid className="w-4" />
               <AlertTitle>Install cognactive to mobile device</AlertTitle>
               <AlertDescription>
-                cognactive is designed to be used as a mobile app during the protocol. Open this webpage from your
-                mobile device. Then open the Share menu and select Add to Home Screen.
+                cognactive should be used as a mobile app during the protocol. Open this webpage from your mobile
+                device. Then open the Share menu <ShareIcon className="inline-block w-4" /> and select &apos;Add to Home
+                Screen&apos;.
               </AlertDescription>
             </Alert>
           )}
-
-          <div className="mt-2 text-sm">
-            By proceeding you agree to:
-            <p className="font-semibold">Privacy Policy</p>
-            All personal event activity data (symptoms, supplements, breakthroughs) is stored locally in web browser and
-            not shared with any third-party.
-            <p className="font-semibold">Legal Disclaimer</p>
-            The cognactive app helps track activities and events and is for personal use only. cognactive is not a
-            medical app. Use this app at your own risk and liability.
-          </div>
+          {isAppInstalled && (
+            <div className="mt-2 p-2 text-sm">
+              By proceeding you agree to:
+              <p className="font-semibold">Privacy Policy</p>
+              All personal event activity data (symptoms, supplements, breakthroughs) is stored locally in web browser
+              and not shared with any third-party.
+              <p className="font-semibold">Legal Disclaimer</p>
+              The cognactive app helps track activities and events and is for personal use only. cognactive is not a
+              medical app. Use this app at your own risk and liability.
+            </div>
+          )}
         </>
       )}
     </div>
