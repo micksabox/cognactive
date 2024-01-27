328fd70b5745930159ffdf3d370dd3ee95609b58
Michael Nolivos <michaelnolivos@gmail.com>
2024-01-27 11:01:37 -0500

    fix: meta at root for pwa app (manifest)

--- app/root.tsx
+++ app/root.tsx
@@ -24,8 +24,28 @@ export default function Root() {
         <meta charSet="utf-8" />
         <Meta />
         <Links />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
+
+        {/* Icons */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+        {/* Open Graph */}
+        <meta property="og:site_name" content="cognactive" />
+        {/* Rest of meta headers can be specified on a per route basis */}
+
+        {/* Twitter previews */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image:alt" content="cognactive" />
+        <meta name="twitter:title" content="cognactive" />
+        <meta name="twitter:description" content="Track NAC Protocol" />
+        <meta name="twitter:image:src" content="/og-image.jpeg" />
+
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#319197" />
       </head>
       <body>
         <div className="h-min-screen">
