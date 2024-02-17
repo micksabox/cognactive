cb9f0fb65dcdc312969f6e72cc8129b92eb4a45a
Michael Nolivos <michaelnolivos@gmail.com>
2024-02-17 14:44:37 -0500

    chore: constant for gh repo

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
 // localstorage key for when user started the NAC protocol in string format yyyy-MM-dd
 export const PROTOCOL_START_DATE = 'protocol_start_date'
+
+// The baseUrl for the Github code repository
+export const GITHUB_REPO_BASE = 'https://github.com/micksabox/cognactive'
