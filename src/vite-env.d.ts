5db4cb66324c72e0ce99f19ae05b3f007f6e88d6
Michael Nolivos <michaelnolivos@gmail.com>
2024-02-26 21:41:20 -0500

    feat: test open graph image-generator

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,15 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
-
+import 'react'
 declare module '*.mdx' {
   let MDXComponent: (props: any) => JSX.Element
   export const frontmatter: any
   export default MDXComponent
+}
+
+// Satisfy Typescript tailwind integration with Satori
+declare module 'react' {
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    tw?: string
+  }
 }
