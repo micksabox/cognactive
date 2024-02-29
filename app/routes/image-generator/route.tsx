067d8afaca1ae614915dda2b61b96f8738e6da68
Michael Nolivos <michaelnolivos@gmail.com>
2024-02-28 20:54:57 -0500

    feat: symbol in image generator and add'tl details

--- app/routes/image-generator/route.tsx
+++ app/routes/image-generator/route.tsx
@@ -3,13 +3,27 @@ import { Resvg } from '@resvg/resvg-js'
 import { LoaderFunctionArgs } from '@remix-run/node'
 import React from 'react'
 
+import fs from 'fs'
+
+export function getLogoBase64() {
+  const logoPath = new URL('../public/logo.png', import.meta.url).pathname
+
+  const logoData = fs.readFileSync(logoPath)
+  return `data:image/png;base64,${logoData.toString('base64')}`
+}
+
 // Used code from https://www.jacobparis.com/content/remix-og
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const jsx = (
     <div tw="flex flex-col p-8 w-full h-full bg-white border border-black">
-      <h1 tw="flex text-6xl">What is NAC Protocol?</h1>
-      <h2 tw="text-4xl text-slate-700">Natural supplements to boost immune and cognitive health</h2>
+      <h1 tw="flex text-7xl">What is NAC Protocol?</h1>
+      <img tw="absolute right-4 bottom-4" src={getLogoBase64()} width={100} height={100} />
+      <h2 tw="text-5xl text-slate-700">Natural supplement stack to boost immune and cognitive health</h2>
+      <p tw="text-3xl">Simple yet powerful anti-fungal cleanse</p>
+      <p tw="text-3xl">- Black seed oil</p>
+      <p tw="text-3xl">- Oregano oil</p>
+      <p tw="text-3xl">- NAC (amino acid)</p>
     </div>
   )
   // From satori docs example
