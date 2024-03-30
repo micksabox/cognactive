3d831f07e590c99ddc2699eb8f006e2ff42c4d17
Michael Nolivos <michaelnolivos@gmail.com>
2024-03-30 13:16:11 -0400

    feat: homepage link to ingread

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,4 +1,4 @@
-import { ActivitySquare, ScrollIcon, ListTodo, HeartHandshakeIcon, LayoutGridIcon } from 'lucide-react'
+import { ActivitySquare, ScrollIcon, ListTodo, HeartHandshakeIcon, LayoutGridIcon, Scan } from 'lucide-react'
 import CognactiveIcon from 'src/assets/icons/cognactive-icon'
 
 import { Button } from '../ui/button'
@@ -40,23 +40,30 @@ const HeroFeature: React.FC<FeatureProps> = ({ icon: Icon, title, description, l
 export const Hero = () => {
   return (
     <div className="flex bg-gradient-to-b from-slate-600 to-slate-900">
-      <section className="w-full py-8">
+      <section className="w-full py-8 text-white">
         <div className="container px-4 md:px-6">
           <div className="grid items-center gap-6 lg:grid-cols-2">
             <div className="flex flex-col justify-center space-y-4 text-center">
               <div className="mb-4">
-                <div id="cognactive-symbol" className="defence-falling m-auto w-48">
+                <div id="cognactive-symbol" className="defence-falling m-auto w-36">
                   <CognactiveIcon className="fill-white" darkMode />
                 </div>
-                <h1 className="my-6 text-3xl font-bold tracking-tighter text-transparent text-white sm:text-5xl xl:text-6xl/none">
+                <h1 className="my-4 text-3xl font-bold tracking-tighter text-transparent text-white sm:text-5xl xl:text-6xl/none">
                   {t['hero-title']}
                 </h1>
+                <p className="mb-4">Open source tools and tech for the anti-fungal NAC protocol.</p>
                 <div className="mx-auto flex max-w-sm flex-col gap-2">
                   <Button className="font-semiboldn bg-cyan py-6 text-lg" size={'lg'} asChild>
                     <Link to="/tracker">
                       <ListTodo className="mr-2" />
                       Launch Tracker
                       {/* {t('get-started')} */}
+                    </Link>
+                  </Button>
+                  <Button className="font-semiboldn py-6 text-lg" size={'lg'} asChild>
+                    <Link to="/ingread">
+                      <Scan className="mr-2" />
+                      Launch Ingredient Scanner
                     </Link>
                   </Button>
                   <Button size={'lg'} variant={'ghost'} className="border border-white text-white shadow-md" asChild>
