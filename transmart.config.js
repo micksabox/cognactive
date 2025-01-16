3f1de81b8a8e611e28f4f3ff5c8cbd8e6c63981b
Michael Nolivos <michaelnolivos@gmail.com>
2025-01-16 12:35:55 -0500

    chore: remove transmart

--- transmart.config.js
+++
@@ -1,9 +0,0 @@
-import { supportedLanguages } from './app/localization/resource'
-
-module.exports = {
-  baseLocale: 'en',
-  locales: supportedLanguages,
-  localePath: 'app/i18n/locales',
-  openAIApiKey: process.env.OPENAI_API_KEY,
-  openAIApiUrl: process.env.OPENAI_API_URL,
-}
